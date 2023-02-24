74480c72f7d4c7ed73ea17f6cd340d2a09b1d49e
Kevin <Kguerrero0325@gmail.com>
2023-02-23 19:54:22 -0500

    myClass Version

--- index.js
+++ index.js
@@ -55,15 +55,57 @@ function setClasses(studentData,schedule){
   })
 
   // append classes to the container
+
+  const zeroPeriod = document.createElement('div')
+  zeroPeriod.textContent = `Zero Period | ${schedule.BellSchedule.Time.ZeroPeriod.S1[0]}-${schedule.BellSchedule.Time.ZeroPeriod.S2[1]}`
+  classesContainer.append(zeroPeriod)
+
+  const homeroom = document.createElement('div')
+  homeroom.textContent = `Homeroom ${schedule.BellSchedule.Time.Homeroom[0]}-${schedule.BellSchedule.Time.Homeroom[1]}`
+  classesContainer.append(homeroom)
+
+  let block = 1
+
   olderList.forEach(myClass => {
     const div = document.createElement('div')
     if(myClass.class === "Lunch"){
-      div.textContent = `${myClass.class}`
+      let timeText = ""
+
+      if(myLunch === "1"){
+        timeText = `${schedule.BellSchedule.Time.FirstLunch[0]}-${schedule.BellSchedule.Time.FirstLunch[1]}`
+      }else if(myLunch === "2"){
+        timeText = `${schedule.BellSchedule.Time.SecondLunch[0]}-${schedule.BellSchedule.Time.SecondLunch[1]}`
+      }
+
+      div.textContent = `${myClass.class} | ${timeText}`
     }else{
-      div.textContent = `${myClass.period}pd - ${myClass.class}`
+      let timeText = ""
+
+      if(block === 1){
+        timeText = `${schedule.BellSchedule.Time.block1[0]}-${schedule.BellSchedule.Time.block1[1]}`
+      }else if(block === 2){
+        timeText = `${schedule.BellSchedule.Time.block2[0]}-${schedule.BellSchedule.Time.block2[1]}`
+      }else if(block === 3){
+        timeText = `${schedule.BellSchedule.Time.block3[0]}-${schedule.BellSchedule.Time.block3[1]}`
+      }else if(block === 4){
+        if(myLunch === "1"){
+          timeText = `${schedule.BellSchedule.Time.BeforeLunch[0]}-${schedule.BellSchedule.Time.BeforeLunch[1]}`
+        }else if(myLunch === "2"){
+          timeText = `${schedule.BellSchedule.Time.AfterLunch[0]}-${schedule.BellSchedule.Time.AfterLunch[1]}`
+        }
+      }else if(block === 5){
+        timeText = `${schedule.BellSchedule.Time.block4[0]}-${schedule.BellSchedule.Time.block4[1]}`
+      }else if(block === 6){
+        timeText = `${schedule.BellSchedule.Time.block5[0]}-${schedule.BellSchedule.Time.block5[1]}`
+      }
+      
+      div.textContent = `${myClass.period}pd - ${myClass.class} | ${timeText}`
+
+      block += 1
     }
     classesContainer.append(div)
   })
+
 }
 
 const App = async() => {
