721b06bd526a91281e0f6b58a8d51656ba6f6432
Kevin <Kguerrero0325@gmail.com>
2023-02-23 19:29:34 -0500

    update
    -added lunch
    -added all periods

--- index.js
+++ index.js
@@ -4,23 +4,52 @@ const currentDayEl = document.getElementById("current-day")
 function setClasses(studentData,schedule){
   let meetClasses = []
   let olderList = []
+  let myLunch = studentData.Lunch[schedule.CurrentDay]
+  let addedLunch = false
 
+  console.log(myLunch)
   // get the classes the student will meet
   studentData.classes.forEach(myClass => {
 
     myClass.meeting.forEach(day => {
       if(day === schedule.CurrentDay){
+
         meetClasses.push(myClass)
+
       }
     })
 
   })
 
+  console.log(meetClasses)
+
   // older sort the student classes
   schedule.DaysMeet[schedule.CurrentDay].forEach(index => {
     meetClasses.forEach(myClass => {
       if(myClass.period === index){
-        olderList.push(myClass)
+         
+        if(olderList.length<3){
+          olderList.push(myClass)
+        }else{
+          if(addedLunch === false){
+            if(myLunch === "1"){
+              olderList.push({
+                "class":"Lunch"
+              })
+              olderList.push(myClass)
+              addedLunch = true
+            }else if(myLunch === "2"){
+              olderList.push(myClass)
+              olderList.push({
+                "class":"Lunch"
+              })
+              addedLunch = true
+            }
+          }else{
+            olderList.push(myClass)
+          }
+        }
+
       }
     })
   })
@@ -28,7 +57,11 @@ function setClasses(studentData,schedule){
   // append classes to the container
   olderList.forEach(myClass => {
     const div = document.createElement('div')
-    div.textContent = `${myClass.period}pd - ${myClass.class}`
+    if(myClass.class === "Lunch"){
+      div.textContent = `${myClass.class}`
+    }else{
+      div.textContent = `${myClass.period}pd - ${myClass.class}`
+    }
     classesContainer.append(div)
   })
 }
